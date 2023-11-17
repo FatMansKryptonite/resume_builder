daaa58d3a1477cd0c6539dcaea0d3401cd308c0b
Ivar Cashin Eriksson <ivar.cashin.eriksson@valcon.com>
2023-11-17 14:59:31 +0100

    Cover letter and CV compiling

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def main():
-    for document in ['cv']:
+    for document in ['cv', 'cover_letter']:
         subprocess.call(['pdflatex',
                          '-output-directory', 'output',
                          '-aux-directory', 'auxiliary_output',
